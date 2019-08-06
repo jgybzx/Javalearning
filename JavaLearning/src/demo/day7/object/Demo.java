be970918760561724ccad735d704c3cdb18e4f6f
jgybzx <1325621332@qq.com>
2019-08-06 18:23:06 +0800

    toString()/hashCode()/equals()方法重写

--- JavaLearning/src/demo/day7/object/Demo.java
+++ JavaLearning/src/demo/day7/object/Demo.java
@@ -8,17 +8,32 @@ package demo.day7.object;
 
 public class Demo {
 	public static void main(String[] args) {
-		Person person=new Person("输出", 12, '男');
-		//直接打印查看输出
+		Person person = new Person("输出", 12, '男');
+		// 直接打印查看输出
 		System.out.println(person);
+		System.out.println(person.toString());
 		/**
-		 *  输出结果:demo.day7.object.Person@4f0ab3f2
-		 *  					      完整类名+ @ +十六进制符
-		 *  
-		 *  github修改提交测试assdsd sas 三点多  收到
-		 *  github修改提交测试2 
-		 *  github修改提交测试3
-		 *  github修改提交测试4
+		 * 输出结果:demo.day7.object.Person@4f0ab3f2 完整类名+ @ +哈希码（十六进制形式） 
+		 * 分析:
+		 * 				1)使用System.out.println(person)打印一个对象时，println()会把对象转换为
+		 * 					一个字符串再打印，调用了String.valueOf(x)方法转换为字符串
+		 * 
+		 * 				2)进一步检查String中的valueOf(x)方法时，发现调用的是toString()方法。
+		 * 
+		 * 				3)toString()是object类中继承而来的，返回内容如下:
+		 * 
+		 * 				return getClass().getName() + "@" + Integer.toHexString(hashCode());
+		 * 						     所以就是： 完整类名 + @ + 哈希码的十六进制形式
+		 * 
+		 * 结论:使用System.out.println(person)打印一个对象时,调用的是对象的toString()方法，由Object继承而来
+		 * 
+		 * 				所以 System.out.println(person) 和
+		 * 				System.out.println(person.toString());返回结果是一样的。
+		 * 
 		 */
+		// 如果利用toString()返回的不是 完整类名 + @ + 哈希码的十六进制形式，那就说明此类重写了这个方法。
+		String text = "asas";
+		System.out.println(text);// 输出:asas，说明String重写了toString.
+		System.out.println(text.hashCode());
 	}
 }
--- JavaLearning/src/demo/day7/object/Person.java
+++ JavaLearning/src/demo/day7/object/Person.java
@@ -10,11 +10,88 @@ public class Person {
 	String name;
 	int age;
 	char sex;
+
 	public Person(String name, int age, char sex) {
 		super();
 		this.name = name;
 		this.age = age;
 		this.sex = sex;
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", sex=" + sex + "]";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + sex;
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		// 向下转型
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sex != other.sex)
+			return false;
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+
 }
