765d630e0fb9487bda129614bd5a46f53cf4f9c6
jgybzx <1325621332@qq.com>
2019-08-13 09:46:41 +0800

    final用法学习

---
+++ JavaLearning/src/demo/day8/finaltest/Father.java
@@ -0,0 +1,11 @@
+/**
+ * @author jgybzx
+ * @data 2019年8月13日 上午9:43:50
+ * @version V1.0
+ * @description 
+ */
+package demo.day8.finaltest;
+
+public final  class Father {
+
+}
---
+++ JavaLearning/src/demo/day8/finaltest/Son.java
@@ -0,0 +1,11 @@
+/**
+ * @author jgybzx
+ * @data 2019年8月13日 上午9:44:08
+ * @version V1.0
+ * @description 
+ */
+package demo.day8.finaltest;
+
+public class Son extends Father {
+
+}
